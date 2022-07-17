79a2b7d7c5cd299a3f10e1efc76b0f845e24222f
Victor Marisa <74539149+victoryeovil@users.noreply.github.com>
2022-07-17 11:36:47 +0200

    Initial commit

---
+++ ecommerce/product/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from .models import Category, Product
+#used to get things from the database to the json file which will display things to the front end
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            "id",
+            "name",
+            "get_absolute_url",
+            "description",
+            "price",
+            "get_image",
+            "get_thumbnail"
+        )
+
+class CategorySerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True)
+
+    class Meta:
+        model = Category
+        fields = (
+            "id",
+            "name",
+            "get_absolute_url",
+            "products",
+        )
